5a21497849cd8f3ce142a50e97dd3c395b840073
quocduong <baoduong2972003@gmail.com>
2024-04-26 11:02:21 +0700

    update nodeJs 74

--- src/controller/doctorController.js
+++ src/controller/doctorController.js
@@ -51,10 +51,25 @@ let getDetailDoctorById = async (req, res) => {
         })
     }
 }
+let bulkCreateSchedule = async (req, res) => {
+    try {
+        let infor = await doctorServies.bulkCreateSchedule(req.body);
+        return res.status(200).json(
+            infor
+        )
+    } catch (e) {
+        console.log(e)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from the server...'
+        })
+    }
+}
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
     getAllDoctors: getAllDoctors,
     postInforDoctor: postInforDoctor,
     getDetailDoctorById: getDetailDoctorById,
+    bulkCreateSchedule: bulkCreateSchedule
 
 }
