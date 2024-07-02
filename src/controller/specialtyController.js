0dc1a8306b0a158eef70b4918d530ca0bedf2080
quocduong <baoduong2972003@gmail.com>
2024-07-02 22:39:44 +0700

    update video 94

--- src/controller/specialtyController.js
+++ src/controller/specialtyController.js
@@ -1,3 +1,4 @@
+import { query } from "express";
 import specialtyService from "../servies/specialtyService";
 
 let createSpecialty = async (req, res) => {
@@ -30,8 +31,22 @@ let getAllSpecialty = async (req, res) => {
         })
     }
 };
-
+let getDetailSpecialtyById = async (req, res) => {
+    try {
+        let infor = await specialtyService.getDetailSpecialtyById(req.query.id, req.query.location);
+        return res.status(200).json(
+            infor
+        )
+    } catch (e) {
+        console.log(e)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from the server...'
+        })
+    }
+}
 module.exports = {
     createSpecialty: createSpecialty,
-    getAllSpecialty: getAllSpecialty
+    getAllSpecialty: getAllSpecialty,
+    getDetailSpecialtyById: getDetailSpecialtyById
 };
