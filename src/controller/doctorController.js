c8bdce6c5e0b906d426202fb4e800c5d92b4d12e
quocduong <baoduong2972003@gmail.com>
2024-07-22 20:41:53 +0700

    update video 98

--- src/controller/doctorController.js
+++ src/controller/doctorController.js
@@ -108,6 +108,20 @@ let getProfileDoctorById = async (req, res) => {
         })
     }
 }
+let getListPatientForDoctor = async (req, res) => {
+    try {
+        let infor = await doctorServies.getListPatientForDoctor(req.query.doctorId, req.query.date);
+        return res.status(200).json(
+            infor
+        )
+    } catch (e) {
+        console.log(e)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from the server...'
+        })
+    }
+}
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
     getAllDoctors: getAllDoctors,
@@ -116,6 +130,7 @@ module.exports = {
     bulkCreateSchedule: bulkCreateSchedule,
     getScheduleByDate: getScheduleByDate,
     getExtraInforDoctorById: getExtraInforDoctorById,
-    getProfileDoctorById: getProfileDoctorById
+    getProfileDoctorById: getProfileDoctorById,
+    getListPatientForDoctor: getListPatientForDoctor
 
 }
